fc6d7134667efb52a88a2b9f81b46d63e58ff798
Fashina Oluwatobi <tobifash2022@gmail.com>
2023-01-22 15:31:05 +0100

    javascript 2

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function switchBulbon() {
+  document.getElementById("bulb").src = "./image/bulb_on.jfif";
+}
+
+function switchBulboff() {
+  document.getElementById("bulb").src = "./image/bulb_off.jfif";
+}
