d7df75835f93b0ede296fec4ee0b56dab8d20610
Fashina Oluwatobi <tobifash2022@gmail.com>
2023-01-24 01:49:22 +0100

    javascript1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
-function switchBulbon() {
-  document.getElementById("bulb").src = "./image/bulb_on.jfif";
-}
+// function switchBulbon() {
+//   document.getElementById("bulb").src = "./image/bulb_on.jfif";
+// }
 
-function switchBulboff() {
-  document.getElementById("bulb").src = "./image/bulb_off.jfif";
-}
+// function switchBulboff() {
+//   document.getElementById("bulb").src = "./image/bulb_off.jfif";
+// }
+
+// function increament() {
+//   let result = document.getElementById("show").innerText;
+//   let x;
+//   console.log(x);
+// }
+// let v = 20;
+// let a = 20;
+// let z = v + a;
+// let z = `the result is ${z}`;
+
+// console.log(result);
+
+// let cars = {
+//   name: "volvo",
+//   year: 2020,
+// };
+
+const studentNames = [
+  [
+    {
+      firstName: "Anyiekpo",
+      lastName: "Asiwaju",
+    },
+
+    { firstName: "Fashina", lastName: "iroko" },
+  ],
+  [
+    {
+      firstName: "Tony",
+      lastName: "Noise",
+    },
+
+    { firstName: "Speed", lastName: "Slow" },
+  ],
+];
+
+console.log(studentNames[2].[0].lastName);
